169ffcf8341e7be9a18e5a52ac1cc73bde22a355
Meir Yeheskel <meir.yeheskel>
2018-03-26 06:55:48 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PeopleinfoComponent } from './peopleinfo/peopleinfo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleinfoComponent
   ],
   imports: [
     BrowserModule
